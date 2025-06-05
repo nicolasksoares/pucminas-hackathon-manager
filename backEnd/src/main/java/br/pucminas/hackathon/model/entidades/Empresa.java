a24d468c84b9c3a317e8ed6a6b1ea3ed7a151ffc
Nicolas Kiffer <nicolask.soares2@gmail.com>
2025-06-04 22:41:24 -0300

    Implementação da classe Empresa
    Implementação da classe Empresa.java

---
+++ backEnd/src/main/java/br/pucminas/hackathon/model/entidades/Empresa.java
@@ -0,0 +1,37 @@
+public class Empresa extends Instituicao {
+    private String tipoEmpresa;
+    private String areaAtuacao;
+    public Empresa(String tipoEmpresa, String areaAtuacao, String nome, String cnpj, String endereco) {
+        super(nome, cnpj, endereco);
+        if (tipoEmpresa == null || tipoEmpresa.trim().length() == 0){
+            throw new IllegalArgumentException("O tipo da empresa não pode estar vazio.");
+        }
+        this.tipoEmpresa = tipoEmpresa.trim();
+
+        if(areaAtuacao == null || areaAtuacao.trim().length() == 0){
+            throw new IllegalArgumentException("A área de atuação não deve estar vazia.");
+        }
+        this.areaAtuacao = areaAtuacao.trim();
+    }
+
+    public String getTipoEmpresa (){
+        return tipoEmpresa;
+    }
+    public String getAreaAtuacao(){
+        return areaAtuacao;
+    }
+
+
+    public void setTipoEmpresa(String tipoEmpresa) {
+        if (tipoEmpresa == null || tipoEmpresa.trim().length() == 0){
+            throw new IllegalArgumentException("O tipo da empresa não pode estar vazio.");
+        }
+        this.tipoEmpresa = tipoEmpresa.trim();
+    }
+    public void setAreaAtuacao(String areaAtuacao){
+        if(areaAtuacao == null || areaAtuacao.trim().length() == 0){
+            throw new IllegalArgumentException("A área de atuação não deve estar vazia.");
+        }
+        this.areaAtuacao = areaAtuacao.trim();
+    }
+}
